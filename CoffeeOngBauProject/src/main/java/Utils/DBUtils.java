9e2cd749062af6c503b281f8206e0296687cb26a
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-29 14:06:30 +0700

    Code kết nối CSDL

--- CoffeeOngBauProject/src/main/java/Utils/DBUtils.java
+++ CoffeeOngBauProject/src/main/java/Utils/DBUtils.java
@@ -1,5 +1,38 @@
 package Utils;
 
-public class DBUtils {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBUtils {
+	public static Connection openConnection() {
+	    Connection con = null;
+	    try {
+	      // Đăng ký driver
+	      Class.forName("com.mysql.cj.jdbc.Driver");
+	      // Chuẩn bị chuỗi kết nối
+	      String connectionURL = "jdbc:mysql://localhost:3306/banhang";
+	      // Mở kết nối
+	      con = DriverManager.getConnection(connectionURL, "root", "");
+	      System.out.println("Connection successful!");
+	    } catch (ClassNotFoundException e) {
+	      System.out.println("MySQL Driver not found!");
+	      e.printStackTrace();
+	    } catch (SQLException e) {
+	      System.out.println("Connection failed!");
+	      e.printStackTrace();
+	    }
+	    return con;
+	  }
+	public static void closeConnection(Connection con) {
+	    if (con != null) {
+	      try {
+	        con.close();
+	        System.out.println("Connection closed!");
+	      } catch (SQLException e) {
+	        System.out.println("Failed to close connection!");
+	        e.printStackTrace();
+	      }
+	    }
+	  }
 }
