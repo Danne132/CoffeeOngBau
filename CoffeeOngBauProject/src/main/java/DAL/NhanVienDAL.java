45118d8dda442623da9d189be43a53ccb08ca954
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-29 14:17:12 +0700

    Thêm các chức năng đăng nhập, đăng ký

--- CoffeeOngBauProject/src/main/java/DAL/NhanVienDAL.java
+++ CoffeeOngBauProject/src/main/java/DAL/NhanVienDAL.java
@@ -1,5 +1,59 @@
 package DAL;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Model_DTO.NhanVien;
+import Utils.ComonUtils;
+import Utils.DBUtils;
+
 public class NhanVienDAL {
+	public boolean AddNew(NhanVien nv) {
+	
+		return true;
+	}
+	public boolean Delete(NhanVien nv) {
+		return true;
+	}
+	public boolean Login(String tk, String mk) throws SQLException {
+		Connection conn = DBUtils.openConnection();
+		
+		String sqlSelect = "SELECT * FROM account";
+		Statement lenh = conn.createStatement();
+		ResultSet ketQua = lenh.executeQuery(sqlSelect);
+		// hiện kết quả
+		while(ketQua.next()) {
+			
+			if(tk.equals(ketQua.getString("taikhoan"))&& 
+					ComonUtils.hashPassword(mk).equals(ketQua.getString("matkhau")) ) {
+				DBUtils.closeConnection(conn);
+				return true;
+			}
+		}
+		
+		DBUtils.closeConnection(conn);
+		return false;
+	}
+	public boolean createAccount(String tk, String mk) throws SQLException {
+		 Connection conn = DBUtils.openConnection();
+		
+		 String sql = "INSERT INTO account (taikhoan,matkhau) VALUES (?, ?)";
+		 PreparedStatement statement = conn.prepareStatement(sql);
+
+         statement.setString(1, tk);    
+         statement.setString(2, ComonUtils.hashPassword(mk)); 
+         int rowsInserted = statement.executeUpdate();
+         if (rowsInserted > 0) {
+             System.out.println("Thêm bản ghi thành công!");
+             DBUtils.closeConnection(conn);
+             statement.close();
+             return true;
+         }
+		DBUtils.closeConnection(conn);
+		return false;
+	}
 
 }
--- CoffeeOngBauProject/src/main/java/Model_DTO/NhanVien.java
+++ CoffeeOngBauProject/src/main/java/Model_DTO/NhanVien.java
@@ -1,5 +1,78 @@
 package Model_DTO;
 
 public class NhanVien {
+	private String maNhanVien;
+    private String hoTen;
+    private int tuoi;
+    private String gioiTinh;
+    private String diaChi;
+    private double luong;
 
+    public NhanVien() {
+    }
+
+    public NhanVien(String maNhanVien, String hoTen, int tuoi, String gioiTinh, String diaChi, double luong) {
+        this.maNhanVien = maNhanVien;
+        this.hoTen = hoTen;
+        this.tuoi = tuoi;
+        this.gioiTinh = gioiTinh;
+        this.diaChi = diaChi;
+        this.luong = luong;
+    }
+    public String getMaNhanVien() {
+        return maNhanVien;
+    }
+
+    public void setMaNhanVien(String maNhanVien) {
+        this.maNhanVien = maNhanVien;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public double getLuong() {
+        return luong;
+    }
+
+    public void setLuong(double luong) {
+        this.luong = luong;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Mã nhân viên: " + maNhanVien);
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Tuổi: " + tuoi);
+        System.out.println("Giới tính: " + gioiTinh);
+        System.out.println("Địa chỉ: " + diaChi);
+        System.out.println("Lương: " + luong);
+    }
 }
