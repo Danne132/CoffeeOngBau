a26dbde0179ddd787300b52781b93a9de0b9b134
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-29 14:22:36 +0700

    Test đăng ký và đăng nhập

--- CoffeeOngBauProject/src/main/java/BLL/NhanVienBLL.java
+++ CoffeeOngBauProject/src/main/java/BLL/NhanVienBLL.java
@@ -1,5 +1,31 @@
 package BLL;
 
-public class NhanVienBLL {
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import DAL.NhanVienDAL;
+import Model_DTO.NhanVien;
+import Utils.DBUtils;
+
+public class NhanVienBLL {
+	NhanVienDAL nhanVienDAL = new NhanVienDAL();
+	boolean ThemMoi(NhanVien nv) {
+		boolean kq = nhanVienDAL.AddNew(nv);
+		return kq;
+	}
+	
+	public boolean dangKiTaiKhoan(String tenDN, String matkhau) throws SQLException{
+		// kiem tra tinh dung dang
+		return nhanVienDAL.createAccount(tenDN, matkhau);
+	}
+	
+	public boolean checkLogin(String tenDN, String matKhau) throws SQLException {
+		//Kiểm tra tính đúng đắn
+		Connection connection = DBUtils.openConnection();
+		
+		//Vd: tên có rỗng không, có đúng format không
+		//Nếu thỏa mãn thì gọi hàm dịch vụ ở tầng DAL
+		return nhanVienDAL.Login(tenDN, matKhau);
+	}
 }
+
--- CoffeeOngBauProject/src/main/java/Main.java
+++ CoffeeOngBauProject/src/main/java/Main.java
@@ -1,9 +1,33 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import BLL.NhanVienBLL;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub
+				NhanVienBLL nv1 = new NhanVienBLL();
+				Scanner scanner = new Scanner(System.in);
+				System.out.print("Tài Khoản: ");
+				String tk = scanner.nextLine();
+				
+				System.out.print("Mật khẩu: ");
+				String mk = scanner.nextLine();
+				
+//				nv1.dangKiTaiKhoan(tk, mk);
+			
+				
+				
+//				System.out.println(ComonUtils.encodePas(mk));
+				
+				if(nv1.checkLogin(tk, mk) == true) {
+					System.out.println("Đăng nhập thành công");
+					
+				}else {
+					System.out.println("Đăng nhập thất bại");
+				}
 	}
 
 }
