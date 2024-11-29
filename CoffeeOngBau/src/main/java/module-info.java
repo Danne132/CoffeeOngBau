6de0101ee359e901133a6c33537076ee7cffc5c7
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-29 15:29:09 +0700

    Tạo giao hiện home
    Test giao diện

--- CoffeeOngBau/src/main/java/module-info.java
+++ CoffeeOngBau/src/main/java/module-info.java
@@ -2,6 +2,6 @@ module project.an.CoffeeOngBau {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens project.an.CoffeeOngBau to javafx.fxml;
+    opens project.an.CoffeeOngBau.Controller to javafx.fxml;
     exports project.an.CoffeeOngBau;
 }
--- CoffeeOngBau/src/main/java/project/an/CoffeeOngBau/App.java
+++ CoffeeOngBau/src/main/java/project/an/CoffeeOngBau/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("fxml/home"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
@@ -26,8 +26,8 @@ public class App extends Application {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    private static Parent loadFXML(String fxmlPath) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxmlPath + ".fxml"));
         return fxmlLoader.load();
     }
 
