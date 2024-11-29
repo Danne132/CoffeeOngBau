b07d78c6e53b6a96dc0778d97bb8100bd0e1b07c
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-11-29 16:02:58 +0700

    Thêm kết nối db, cập nhật file

--- CoffeeOngBau/src/main/java/module-info.java
+++ CoffeeOngBau/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module project.an.CoffeeOngBau {
     requires javafx.controls;
     requires javafx.fxml;
+	requires java.sql;
 
     opens project.an.CoffeeOngBau.Controller to javafx.fxml;
     exports project.an.CoffeeOngBau;
