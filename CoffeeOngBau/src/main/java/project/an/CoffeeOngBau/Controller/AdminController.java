6c7ebe738f417345a8c57825c43a320845569e11
Danne132 <an.hd.64cntt@ntu.edu.vn>
2024-12-17 14:43:53 +0700

    update code lấy ảnh từ máy tính

--- CoffeeOngBau/src/main/java/project/an/CoffeeOngBau/Controller/AdminController.java
+++ CoffeeOngBau/src/main/java/project/an/CoffeeOngBau/Controller/AdminController.java
@@ -7,14 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import project.an.CoffeeOngBau.Models.Entities.LoaiSP;
 import project.an.CoffeeOngBau.Models.Entities.SanPham;
 import project.an.CoffeeOngBau.Models.Entities.currentAccount;
 import project.an.CoffeeOngBau.Utils.DBUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
@@ -46,19 +50,19 @@ public class AdminController implements Initializable {
     private Button productCategoryAddBtn;
 
     @FXML
-    private TableColumn<?, ?> productColDonGia;
+    private TableColumn<SanPham, String> productColDonGia;
 
     @FXML
-    private TableColumn<?, ?> productColLoaiSP;
+    private TableColumn<SanPham, String> productColLoaiSP;
 
     @FXML
-    private TableColumn<?, ?> productColMaSP;
+    private TableColumn<SanPham, String> productColMaSP;
 
     @FXML
-    private TableColumn<?, ?> productColTenSP;
+    private TableColumn<SanPham, String> productColTenSP;
 
     @FXML
-    private TableColumn<?, ?> productColTrangThai;
+    private TableColumn<SanPham, String> productColTrangThai;
 
     @FXML
     private Button productDeleteBtn;
@@ -79,7 +83,7 @@ public class AdminController implements Initializable {
     private Button productImageImport;
 
     @FXML
-    private ComboBox<?> productLoaiSPCBB;
+    private ComboBox<String> productLoaiSPCBB;
 
     @FXML
     private TextField productMaSPText;
@@ -91,13 +95,13 @@ public class AdminController implements Initializable {
     private Button productNavBtn;
 
     @FXML
-    private TableView<?> productTable;
+    private TableView<SanPham> productTable;
 
     @FXML
     private TextField productTenSPText;
 
     @FXML
-    private ComboBox<?> productTrangThaiCBB;
+    private ComboBox<String> productTrangThaiCBB;
 
     @FXML
     private Button productUpdateBtn;
@@ -114,13 +118,25 @@ public class AdminController implements Initializable {
     private HashMap<String, String> loaisps = new HashMap<>();
     private String[] trangthaisps = new String[]{"Đang bán", "Ngưng bán"};
     private Connection conn;
-
+    private ObservableList<SanPham> sanPhams;
+    private Image image;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         displayName();
         getCategoryFromDB();
         getProductStatus();
+        showSPList();
+    }
+
+    public void showSPList(){
+        sanPhams = getSPList();
+        productColMaSP.setCellValueFactory(new PropertyValueFactory<>("maSP"));
+        productColTenSP.setCellValueFactory(new PropertyValueFactory<>("tenSP"));
+        productColLoaiSP.setCellValueFactory(new PropertyValueFactory<>("loaiSP"));
+        productColDonGia.setCellValueFactory(new PropertyValueFactory<>("donGia"));
+        productColTrangThai.setCellValueFactory(new PropertyValueFactory<>("trangThai"));
+        productTable.setItems(sanPhams);
     }
 
     public ObservableList<SanPham> getSPList(){
@@ -139,15 +155,18 @@ public class AdminController implements Initializable {
                         ketQua.getString("loaiSP"),
                         ketQua.getString("moTa"),
                         ketQua.getString("ghiChu"),
-                        ketQua.getString("")
-                )
+                        ketQua.getString("trangThai"),
+                        ketQua.getString("anhSP"),
+                        ketQua.getDouble("donGia")
+                );
+                spList.add(sp);
             }
-            ObservableList list = FXCollections.observableArrayList(loaisps.values());
-            productLoaiSPCBB.setItems(list);
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
         DBUtils.closeConnection(conn);
+        return spList;
     }
 
     @FXML
@@ -197,6 +216,7 @@ public class AdminController implements Initializable {
             }
             ObservableList list = FXCollections.observableArrayList(loaisps.values());
             productLoaiSPCBB.setItems(list);
+            productLoaiSPCBB.getSelectionModel().select(0);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -210,5 +230,28 @@ public class AdminController implements Initializable {
         }
         ObservableList list = FXCollections.observableArrayList(listTT);
         productTrangThaiCBB.setItems(list);
+        productTrangThaiCBB.getSelectionModel().select(0);
+    }
+
+    public void addSP(){
+        if(productTenSPText.getText().isEmpty() ||
+                productDonGiaText.getText().isEmpty()||
+                currentAccount.path == null){
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Lỗi thêm sản phẩm");
+            alert.setHeaderText(null);
+            alert.setContentText("Hãy điền đủ thông tin sản phẩm");
+        }
+    }
+
+    public void importImage(){
+        FileChooser openFile = new FileChooser();
+        openFile.getExtensionFilters().add(new FileChooser.ExtensionFilter("Open Image File", "*png", "*jpg"));
+        File file = openFile.showOpenDialog(adminForm.getScene().getWindow());
+        if(file != null){
+            currentAccount.path = file.getAbsolutePath();
+            image = new Image(file.toURI().toString(), 113, 125, false, true);
+            productImage.setImage(image);
+        }
     }
 }
